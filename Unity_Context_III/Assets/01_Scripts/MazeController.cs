533a84da0dbc247e7f7e293480e04a371bb2eb89
Marc Rozendaal <rozendaalmarc@gmail.com>
2023-11-07 14:29:30 +0100

    Added basic maze movement

---
+++ Unity_Context_III/Assets/01_Scripts/MazeController.cs
@@ -0,0 +1,21 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject maze;
+    
+    [SerializeField]
+    private Vector3 dir;
+
+    [SerializeField, Range(-90, 90)]
+    private float x, y, z;
+
+    private void Update() {
+        dir = new Vector3(x, y, z);
+        maze.transform.eulerAngles = dir;
+    }
+}
